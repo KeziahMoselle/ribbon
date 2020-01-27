4696ee7694a8a0130a6c7591ff367287a66a9d59
Nemesis <keziah.moselle@gmail.com>
2020-01-27 18:45:07 +0100

    feat: material bottom tabs
    * chore: remove deps
    * feat: material bottom tabs

--- App.tsx
+++ App.tsx
@@ -1,18 +1,38 @@
 import React from 'react';
 import AppProviders from './components/providers/Context';
+import { createAppContainer } from 'react-navigation';
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
+import { Feather } from '@expo/vector-icons';
+
 import HomeScreen from './screens/HomeScreen';
 import SettingsScreen from './screens/SettingsScreen';
-import { createAppContainer } from 'react-navigation';
-import { createBottomTabNavigator } from 'react-navigation-tabs';
 import StatusBar from './components/Layout/StatusBar';
 
-
-const TabNavigator = createBottomTabNavigator({
-  Home: HomeScreen,
-  Settings: SettingsScreen,
-}, {
-  initialRouteName: 'Home'
-});
+const TabNavigator = createMaterialBottomTabNavigator(
+  {
+    Bookmarks: {
+      screen: HomeScreen,
+      navigationOptions: {
+        tabBarColor: '#000',
+        tabBarIcon: <Feather name="bookmark" size={20} color="#FFF" />
+      }
+    },
+    Settings: {
+      screen: SettingsScreen,
+      navigationOptions: {
+        tabBarColor: '#333',
+        tabBarIcon: <Feather name="settings" size={20} color="#FFF" />
+      }
+    }
+  },
+  {
+    shifting: true,
+    initialRouteName: 'Bookmarks',
+    activeColor: '#FFF',
+    inactiveColor: '#EEE',
+    barStyle: { backgroundColor: '#000' }
+  }
+)
 
 const AppContainer = createAppContainer(TabNavigator);
 
