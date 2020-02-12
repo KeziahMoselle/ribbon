0633cd0eafe66b2d49f3cedf0525e65cc922f342
KeziahMoselle <keziah.moselle@gmail.com>
2020-02-12 20:45:24 +0100

    feat: simple gesture

--- components/Bookmark/ImageBookmark.tsx
+++ components/Bookmark/ImageBookmark.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Image } from 'react-native';
 import Animated from 'react-native-reanimated';
 import { PanGestureHandler, State } from 'react-native-gesture-handler';
@@ -11,6 +11,8 @@ import {
 import { Linking } from 'expo';
 import usePinnedBookmarks from '../providers/hooks/usePinnedBookmarks';
 import { useBookmarks } from '../providers/BookmarksProvider';
+
+const { Value, event, cond, eq, call, or } = Animated;
 
 interface Props extends BookmarkInterface {
   index: number;
@@ -28,33 +30,104 @@ function Bookmark({
   url,
   index
  }: Props) {
-   const { isPinned, handlePinClick } = usePinnedBookmarks({
-     id,
-     index,
-     title,
-     permalink
-   });
+  const { isPinned, handlePinClick } = usePinnedBookmarks({
+    id,
+    index,
+    title,
+    permalink
+  });
+  const { unsaveBookmark } = useBookmarks();
 
-   const { unsaveBookmark } = useBookmarks();
+  const [dragging, setDragging] = useState(false);
+  const [x, setX] = useState(0);
+  const gestureState = new Value(-1);
+  const offsetX = new Value(0);
+
+  const onGestureEvent = event([
+    {
+      nativeEvent: {
+        translationX: offsetX,
+        state: gestureState
+      }
+    }
+  ])
+
+  function start([]) {
+    setDragging(true);
+  }
+
+  function moving([offsetX]) {
+    setX(offsetX);
+    console.log(offsetX);
+  }
+
+  function done([offsetX]) {
+    setDragging(false);
+
+    // Open the actions UI
+    if (offsetX < -40) return setX(-160);
+    // Close the actions UI
+    if (offsetX > -40) return setX(0);
+  }
 
   return (
     <View style={styles.container}>
-      <View style={{ zIndex: 2 }}>
-        <View style={[styles.header, styles.padding]}>
-          <Title>{ subreddit }</Title>
-          <Caption>{ date } ago</Caption>
-        </View>
+      <Animated.Code>
+        {() => 
+          cond(
+            eq(gestureState, State.BEGAN),
+            call([], start)
+          )
+        }
+      </Animated.Code>
 
-        <TouchableRipple
-          onPress={() => Linking.openURL(permalink || url)}
-          style={styles.imageContainer}
-        >
-          <Image
-            source={{ uri: thumbnail }}
-            style={styles.image}
-          />
-        </TouchableRipple>
-      </View>
+      <Animated.Code>
+        {() =>
+          cond(
+            eq(gestureState, State.ACTIVE),
+            call([offsetX], moving)
+          )
+        }
+      </Animated.Code>
+
+      <Animated.Code>
+        {() =>
+          cond(
+            or(
+              eq(gestureState, State.END),
+              eq(gestureState, State.FAILED),
+              eq(gestureState, State.CANCELLED)
+            ),
+            call([offsetX], done)
+          )
+        }
+      </Animated.Code>
+
+      <PanGestureHandler
+        maxPointers={1}
+        onGestureEvent={onGestureEvent}
+        onHandlerStateChange={onGestureEvent}
+      >
+        <Animated.View style={{
+           zIndex: 2,
+           translateX: x
+        }}>
+          <View style={[styles.header, styles.padding]}>
+            <Title>{ subreddit }</Title>
+            <Caption>{ date } ago</Caption>
+          </View>
+
+          <TouchableRipple
+            onPress={() => Linking.openURL(permalink || url)}
+            style={styles.imageContainer}
+          >
+            <Image
+              source={{ uri: thumbnail }}
+              style={styles.image}
+            />
+          </TouchableRipple>
+        </Animated.View>
+      </PanGestureHandler>
 
       <View style={{
         zIndex: 1,
